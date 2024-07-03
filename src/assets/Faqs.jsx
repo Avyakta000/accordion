a46821671f890076af6259c693e43355f13af493
Avyakta000 <100035215+Avyakta000@users.noreply.github.com>
2024-07-03 21:52:03 +0530

    update 1

--- src/assets/Faqs.jsx
+++ src/assets/Faqs.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaChevronUp, FaChevronDown } from 'react-icons/fa6'
+import { FiMinusCircle, FiPlusCircle } from 'react-icons/fi';
 
 
 export const Faqs = ({ question, active, answer, onClick }) => {
@@ -12,8 +12,8 @@ export const Faqs = ({ question, active, answer, onClick }) => {
             <li className='py-3 w-full h-fit'>
                 <div className='flex justify-between' onClick={toggle}>
                     <h2 className={`w-70% max-sm:w-72 font-semibold hover:text-[#ff61c8] cursor-pointer ${active ? 'text-[#ff61c8]' : 'text-[#000000]'}`} >{question}</h2>
-                    <button>
-                        {active ? <FaChevronUp  /> : <FaChevronDown  />}
+                    <button className={active ? "text-pink-500" : ""}>
+                        {active ? <FiMinusCircle  /> : <FiPlusCircle  />}
                     </button>
                 </div>
                 <p className={`text-start py-3 mb-3 leading-tight ${active ? 'block' : 'hidden'} text-[#3f3c3e]`}>{answer}</p>
